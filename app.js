e5232d1ebcdf36a77d21af083615155c83218eee
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-09-25 10:15:43 +0100

    initialize socketio

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server); // initialize socketio with server
+
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
-const app = express();
+
 
 // middlewares
 app.use(cors());
@@ -11,9 +18,17 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json());
 
+// Use static files from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', socket => { // connect io to listen for connection
+    console.log("New websocket connection");
+})
 
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
     console.log(" this app runs on port 3000");
 })
 
