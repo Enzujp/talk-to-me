9c73c335a603ce3ad9f6a5f0a285369bdf9594ae
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-11-20 17:25:39 +0100

    authentication complete

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,6 +8,42 @@ const createToken = (id) => {
     return jwt.sign({id}, process.env.SECRET_KEY, { expiresIn: '2h'});
 }
 
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { username: '', password: '' };
+  
+    // duplicate email error
+    if (err.code === 11000) {
+      errors.username = 'this username is already registered';
+      return errors;
+    }
+  
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+      // console.log(err);
+      Object.values(err.errors).forEach(({ properties }) => {
+        // console.log(val);
+        // console.log(properties);
+        errors[properties.path] = properties.message;
+      });
+  
+      // incorrect email
+      if (err.message === "Incorrect Email Address Entered") {
+        errors.email = "That email is not registered"
+      }
+  
+      // incorrect password
+      if (err.message === "Incorrect Passoword") {
+        err.password = "Your Password is Incorrect."
+      }
+    }
+  
+    return errors;
+  }
+
+
+// Get  signup page
 module.exports.signup_get = (req, res) => {
     res.render("signup");
 }
@@ -47,8 +83,10 @@ module.exports.signup_post = async(req, res) => {
                     })
                 })
                 .catch(err => {
+                    const errors = handleErrors(err)
+                    console.log(errors)
                     res.status(500).json({
-                        err: err,
+                        errors: errors,
                         message: "Find me here, I'm an error"
                     })
                 })
